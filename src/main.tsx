3f3e44bb81f2e0274802d77771e9b0c977749935
Ojas37 <ojassachinneve@gmail.com>
2026-06-23 19:20:00 +0530

    perf: optimize font loading by self-hosting google fonts locally via fontsource

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@fontsource/anton";
+import "@fontsource-variable/montserrat";
+import "@fontsource-variable/montserrat/wght-italic.css";
 import './index.css'
 import App from './App.tsx'
 
---
+++ src/types.d.ts
@@ -0,0 +1,3 @@
+declare module "@fontsource/anton";
+declare module "@fontsource-variable/montserrat";
+declare module "@fontsource-variable/montserrat/wght-italic.css";
